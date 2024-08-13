09f85a89114b38b95a36a478778c1fea36f18dfb
retrymp3 <retrymp492@gmail.com>
2024-08-13 20:59:35 +0530

    Optimized file reader using BufReader

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,32 @@
 use clap::Parser;
+use std::path::PathBuf;
+use std::io::{self, BufReader, BufRead};
+use std::fs::File;
 
 #[derive(Parser)]
 struct Cli {
     pattern: String,
-    path: std::path::PathBuf,
+    path: PathBuf,
 }
 
-fn main() {
+fn main() -> io::Result<()> {
     let args = Cli::parse();
-    let data = std::fs::read_to_string(&args.path).expect("Unable to read.");
+    let f = File::open(args.path)?;
 
-    for l in data.lines(){
-        if l.contains(&args.pattern){
-            println!("{}",l);
+    let mut reader = BufReader::new(f);
+    let mut line = String::new();
+
+    loop {
+        let lb = reader.read_line(&mut line)?;
+        
+        if lb == 0{
+            break;
         }
+        else if line.trim().contains(&args.pattern){
+            println!("{}",line.trim());
+        }
+    line.clear();
     }
+    Ok(())
+
 }
